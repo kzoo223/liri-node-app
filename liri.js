98e82b5f25b4771d9e0c4bb50ff45f105595ec6f
Kevin Zhu <kzoo223@gmail.com>
2016-10-09 17:23:24 -0400

    twitter functional, need to adjust tweet limit

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 //Set up requires
-var tKeys = require("keys.js");
+var tKeys = require("./keys.js");
 var fs = require("fs");
 var request = require("request");
-var twitter = requre("twitter");
+var twitter = require("twitter");
 var spotify = require("spotify");
 
 //set up variables
@@ -11,7 +11,27 @@ var item = process.argv[3];
 
 
 //set up twitter client
-var tClient = new twitter(
-  keys.twitterKeys
-)
+var tClient = new twitter(tKeys.twitterKeys);
+
+
+//commands processing
+if(command=="my-tweets") {
+  getTwitter();
+}
+
+//twitter function
+function getTwitter(){
+var params = {screen_name: 'ZooJerzey'};
+tClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+      for (var i=0;i<tweets.length;i++){
+      console.log("Tweeted: "+ tweets[i].text + " on: "+ tweets[i].created_at)
+  }
+}
+});
+}
+
+
+
+
 
