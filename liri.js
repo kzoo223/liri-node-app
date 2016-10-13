8b6e4756f4131bb42472d03007429418b8969891
Kevin Zhu <kzoo223@gmail.com>
2016-10-12 23:37:05 -0400

    spotify working

--- liri.js
+++ liri.js
@@ -14,24 +14,39 @@ var item = process.argv[3];
 var tClient = new twitter(tKeys.twitterKeys);
 
 
-//commands processing
-if(command=="my-tweets") {
-  getTwitter();
-}
-
 //twitter function
 function getTwitter(){
 var params = {screen_name: 'ZooJerzey'};
 tClient.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-      for (var i=0;i<tweets.length;i++){
+      for (i=0;i<20;i++){
       console.log("Tweeted: "+ tweets[i].text + " on: "+ tweets[i].created_at)
   }
 }
 });
 }
 
+//spotify function
+function spotifySong(){
 
+  spotify.search({ type: 'track', query: item}, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+      }
+        var search = data.tracks.items[0]
+        console.log("\nSong: " + search.name + "\nArtist: "+search.artists[0].name + "\nAlbum: " + search.album.name + "\nLink: " + search.preview_url)
+         
+});
+}
+
+//commands processing
+if(command=="my-tweets") {
+  getTwitter();
+}
+else if(command=="spotify-this-song") {
+  spotifySong();
+}
 
 
 
